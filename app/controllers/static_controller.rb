b293d639c6e7486eaa7bb33c7e0717a152218e01
John Newcomb <jnewcs@stanford.edu>
2015-11-15 19:00:25 -0800

    Fixing static controller format

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,9 +6,19 @@ class StaticController < ApplicationController
     @questions = SurveyQuestion.all_questions
   end
 
+  def quiz1
+  end
+
+  def quiz2
+  end
+
+  def quiz3
+  end
+
   def matches
   end
 
   def match_detail
   end
+
 end
